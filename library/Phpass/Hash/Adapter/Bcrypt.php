a8033c4fd4c62cafa72ef1ed1b27335095040042
Ryan Chouinard <rchouinard@gmail.com>
2012-03-27 22:29:12 -0400

    Add option to change identifier in bcrypt adapter.

--- library/Phpass/Hash/Adapter/Bcrypt.php
+++ library/Phpass/Hash/Adapter/Bcrypt.php
@@ -14,6 +14,7 @@
  * @namespace
  */
 namespace Phpass\Hash\Adapter;
+use Phpass\Exception\InvalidArgumentException;
 
 /**
  * PHPass Bcrypt Hash Adapter
@@ -29,6 +30,29 @@ class Bcrypt extends Base
 {
 
     /**
+     * String identifier to use when generating new hash values.
+     *
+     * @var string
+     */
+    protected $_identifier = '2a';
+
+    /**
+     * Logarithmic cost value used when generating new hash values.
+     *
+     * @var integer
+     */
+    protected $_iterationCountLog2 = 12;
+
+    /**
+     * List of valid hash identifier strings.
+     *
+     * @var array
+     */
+    protected $_validIdentifiers = array ('2a', '2x', '2y');
+
+    /**
+     * Class constructor.
+     *
      * @see \Phpass\Hash\Base::__construct()
      */
     public function __construct(Array $options = array ())
@@ -38,6 +62,59 @@ class Bcrypt extends Base
     }
 
     /**
+     * Set adapter options.
+     *
+     * Expects an associative array of option keys and values used to configure
+     * the hash adapter instance.
+     *
+     * <dl>
+     *   <dt>iterationCountLog2</dt>
+     *     <dd>A logarithmic value between 4 and 31, inclusive. This value
+     *     used to calculate the cost factor associated with generating a new
+     *     hash value. A higher number means a higher cost, with each increment
+     *     doubling the cost. Defaults to 12.</dd>
+     *   <dt>identifier</dt>
+     *     <dd>Hash identifier to use when generating a new hash value.
+     *     Supported identifiers are 2a, 2x, and 2y. Defaults to 2a.</dd>
+     * </dl>
+     *
+     * @param array $options
+     *   Associative array of adapter options.
+     * @return void
+     */
+    public function setOptions(Array $options)
+    {
+        parent::setOptions($options);
+
+        $options = array_change_key_case($options, CASE_LOWER);
+        foreach ($options as $key => $value) {
+            switch ($key) {
+                case 'iterationcountlog2':
+                    $value = (int) $value;
+                    if ($value <= 4 || $value >= 31) {
+                        throw new InvalidArgumentException('Iteration count must be a logarithmic value between 4 and 31');
+                    }
+                    $this->_iterationCountLog2 = $value;
+                    break;
+                case 'identifier':
+                    if (!in_array($value, $this->_validIdentifiers)) {
+                        throw new InvalidArgumentException('Invalid hash identifier.');
+                    }
+                default:
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Generate a salt string suitable for PHP's crypt() function
+     *
+     * Bcrypt::genSalt() generates a 29-character salt string which can be
+     * passed to crypt() in order to use the CRYPT_BLOWFISH hash type. The salt
+     * consists of a string beginning with a compatible hash identifier, a
+     * two-digit cost factor, and a 22-character encoded salt string using the
+     * characters "./0-9A-Za-z", separated by "$".
+     *
      * @see \Phpass\Hash\Adapter::genSalt()
      */
     public function genSalt($input = null)
@@ -46,16 +123,15 @@ class Bcrypt extends Base
             $input = $this->_getRandomBytes(16);
         }
 
-        // Blowfish requires this number to be between 4 and 31.
-        $countLog2 = ($this->_iterationCountLog2 < 4)
-            ? 4
-            : min($this->_iterationCountLog2, 31);
+        // Hash identifier
+        $output = '$' . $this->_identifier . '$';
 
-        $output = '$2a$';
-        $output .= chr(ord('0') + $countLog2 / 10);
-        $output .= chr(ord('0') + $countLog2 % 10);
+        // Cost factor
+        $output .= chr(ord('0') + $this->_iterationCountLog2 / 10);
+        $output .= chr(ord('0') + $this->_iterationCountLog2 % 10);
         $output .= '$';
 
+        // Random salt data
         $i = 0;
         do {
             $c1 = ord($input[$i++]);
@@ -77,6 +153,7 @@ class Bcrypt extends Base
             $output .= $this->_itoa64[$c2 & 0x3f];
         } while (1);
 
+        // $II$CC$SSSSSSSSSSSSSSSSSSSSSS
         return $output;
     }
 
